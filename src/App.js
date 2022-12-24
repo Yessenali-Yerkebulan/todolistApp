944bdea313185d4f0ee6c7044ce42597c314a2af
Yessenali-Yerkebulan <yerkebulan.yessenali@narxoz.kz>
2022-12-24 03:56:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+import './App.css';
+import Header from './components/Header/Header';
+import AddTodo from './components/AddTodo/AddTodo';
+import TodoList from './components/TodoList/TodoList';
+import {Container} from 'react-bootstrap';
+function App() {
+  function getLocalItems(){
+    let todo = localStorage.getItem('todo');
+    console.log(todo)
+    if(todo){
+      return JSON.parse(localStorage.getItem('todo'));
+    }else{
+      return [];
+    }
+  }
+
+  const [todo, setTodo] = useState(getLocalItems());
+ 
+  return (
+    <Container>
+        <Header />
+        <AddTodo todo={todo} setTodo={setTodo}/>
+        <TodoList todo={todo} setTodo={setTodo}/>
+    </Container>
+  );
+}
+
+export default App;
